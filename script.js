6fe0a8871b103e5580b63c71355137c1b85db342
Anupam311 <chaudharyanupam746@gmail.com>
2026-03-14 21:48:22 +0530

    Added js integration and updated css styling

---
+++ script.js
@@ -0,0 +1,33 @@
+// Smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        document
+            .querySelector(this.getAttribute("href"))
+            .scrollIntoView({
+                behavior: "smooth"
+            });
+
+    });
+
+});
+
+
+
+// Contact form message
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    alert("Message sent successfully!");
+
+    form.reset();
+
+});
